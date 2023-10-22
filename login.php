6a2a23e89cdcea79faa999b3ae4c95f3f1a8339f
sircarion <148720771+sircarion@users.noreply.github.com>
2023-10-22 19:44:10 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,9 +1,12 @@
 <?php
-require 'conexão.php'; // Inclua o arquivo de conexão com o banco de dados
+require 'conexao.php'; // Inclua o arquivo de conexão com o banco de dados
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
+
+    // Aplicar criptografia à senha (se sua tabela de usuários tiver uma coluna 'senha' para armazenar senhas criptografadas)
+    $hashed_password = password_hash($password, PASSWORD_BCRYPT);
 
     $sql = "SELECT * FROM usuarios WHERE nome_usuario = ?"; // Substitua "usuarios" pelo nome da tabela de usuários no seu banco de dados
 
@@ -18,7 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if ($result->num_rows === 1) {
         $row = $result->fetch_assoc();
 
-        // Verificar a senha criptografada
+        // Verificar a senha criptografada (se sua tabela de usuários armazena senhas criptografadas)
         if (password_verify($password, $row['senha'])) {
             // Login bem-sucedido, redirecione para a página principal
             header("Location: pagina_principal.php");
@@ -33,5 +36,28 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $stmt->close();
 }
 
+// Manipulação de entidades (exemplo: inserir um paciente)
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $nome_paciente = $_POST['nome_paciente'];
+
+    $sql = "INSERT INTO paciente (nome) VALUES (?)";
+
+    // Preparar a declaração
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $nome_paciente);
+
+    // Executar a inserção
+    if ($stmt->execute()) {
+        echo "Paciente inserido com sucesso.";
+    } else {
+        echo "Erro ao inserir paciente: " . $conn->error;
+    }
+
+    $stmt->close();
+}
+
+// Adicione lógica semelhante para outras operações nas demais entidades (Médico, Material, Medicamentos, Fornecedores)
+
 $conn->close();
 ?>
+
