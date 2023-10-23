1c5d5276c013815d0d6561fb749fbd365dc16b12
sircarion <148720771+sircarion@users.noreply.github.com>
2023-10-23 18:55:23 -0300

    hospital.sql

--- hospital.sql
+++ hospital.sql
@@ -2,6 +2,15 @@ create database hospital;
 
 use hospital;
 
+CREATE TABLE paciente (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    cpf VARCHAR(14) NOT NULL,
+    data_nascimento DATE NOT NULL,
+    email VARCHAR(255) NOT NULL,
+);
+
 CREATE TABLE medicos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     senha VARCHAR(255) NOT NULL,
